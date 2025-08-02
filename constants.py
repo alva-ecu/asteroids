6274b53b7d8af324f638a31c72dc2b9caf6232e7
alva-ecu <baru7279@gmail.com>
2025-08-01 20:02:06 -0500

    Added a brake function, changed player constants for a floaty feel

--- constants.py
+++ constants.py
@@ -8,7 +8,9 @@ ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS
 
 PLAYER_RADIUS = 20
 PLAYER_TURN_SPEED = 300
-PLAYER_SPEED = 200
+PLAYER_ACCELERATION = 400   # How fast the player accelerates
+PLAYER_MAX_SPEED = 400      # Maximum speed the player can reach
+PLAYER_FRICTION = 0.75      # How quickly the player slows down (higher = more friction)
 PLAYER_SHOT_SPEED = 500
 PLAYER_SHOOT_COOLDOWN = 0.3
 
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOT_SPEED, PLAYER_SHOOT_COOLDOWN
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_ACCELERATION, PLAYER_MAX_SPEED, PLAYER_FRICTION, PLAYER_SHOT_SPEED, PLAYER_SHOOT_COOLDOWN
 from shot import Shot
 
 class Player(CircleShape):
@@ -9,6 +9,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 180
         self.timer = 0
+        self.velocity = pygame.Vector2(0, 0)  # Add velocity tracking
     
     # in the player class
     def triangle(self):
@@ -24,11 +25,22 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation += (PLAYER_TURN_SPEED * dt)
-    
-    def move(self, dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
 
+    def accelerate(self, dt, direction):
+        # Calculate thrust in the direction the player is facing
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        thrust = forward * PLAYER_ACCELERATION * direction * dt
+        self.velocity += thrust
+
+        # Cap velocity at maximum speed
+        if self.velocity.length() > PLAYER_MAX_SPEED:
+            self.velocity = self.velocity.normalize() * PLAYER_MAX_SPEED
+
+    def brake(self, dt):
+        # Apply extra friction when braking
+        brake_strength = PLAYER_FRICTION * 2  # Make braking X times stronger than normal friction
+        self.velocity *= (1 - brake_strength * dt)
+    
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -37,13 +49,20 @@ class Player(CircleShape):
         if keys[pygame.K_d]:
             self.rotate(dt)
         if keys[pygame.K_s]:
-            self.move(-dt) 
+            self.brake(dt) 
         if keys[pygame.K_w]:
-            self.move(dt)
+            self.accelerate(dt, 1)
         if keys[pygame.K_SPACE]:
             if self.timer <= 0:
                 self.shoot()
                 self.timer = PLAYER_SHOOT_COOLDOWN
+        
+        # Apply friction to gradually slow down
+        self.velocity *= (1 - PLAYER_FRICTION * dt)
+        
+        # Update position based on current velocity
+        self.position += self.velocity * dt
+        
         self.timer -= dt
      
     def shoot(self):
